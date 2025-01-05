07b0351e14986dabbb7af8ad47e72bbee4c5b7d6
Shandeep Raula <147018064+Shandeep-Raula@users.noreply.github.com>
2025-01-05 21:07:56 +0530

    Update move_to_postgres.py

--- Python/move_to_postgres.py
+++ Python/move_to_postgres.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+# function for Postgres database connection 
 def move_to_postgres(dataframe, table_name, db_name, user, password, host, port):
     connection_string = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
     engine = create_engine(connection_string)
     dataframe.to_sql(table_name, engine, if_exists='replace', index='False')
 
+# read all the files and change the datatype
 city_target = pd.read_csv('city_target_passenger_rating.csv')
 
 dim_city = pd.read_csv('dim_city.csv')
@@ -29,6 +31,8 @@ monthly_target['month'] = pd.to_datetime(monthly_target['month'])
 monthly_target_trips = pd.read_csv('monthly_target_trips.csv')
 monthly_target_trips['month'] = pd.to_datetime(monthly_target_trips['month'])
 
+
+# Parameters for database connection
 db_name = "Goodcabs"
 user = "postgres"
 password = "subu2003"
@@ -36,7 +40,7 @@ host = "localhost"
 port = 5432
 
 
-
+# Load all the files to Postgres database
 move_to_postgres(city_target, "city_target_passenger_rating", db_name, user, password, host, port)
 move_to_postgres(dim_city, "dim_city", db_name, user, password, host, port)
 move_to_postgres(dim_date, "dim_date", db_name, user, password, host, port)
